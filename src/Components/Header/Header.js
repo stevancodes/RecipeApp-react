9473d7e860e9a89cd399e9f91ddedf0f30fec06c
stevancodes <stevan.code@gmail.com>
2022-06-27 12:36:12 +0200

    new link route

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -16,48 +16,13 @@ function Header() {
 
     return (
         <div className="Header">
-            <h1>Coolinarka</h1>
+            <Link to="/"><h1>Coolinarka</h1></Link>
             <div className="inside-wrapper">
                 <Link to="/">Recipes</Link>
                 <button onClick={openRecipeModal}>Create Recipe</button>
             </div>
             {newRecipeModal && <CreateRecipeModal update={update} openRecipeModal={openRecipeModal} />}
         </div>
-        // <div className="header">
-        //     <nav className="navbar" role="navigation" aria-label="main navigation">
-        //         <div className="website-title is-12">IVANOV KUVAR</div>
-
-        //         <div id="navbarBasicExample" className="navbar-menu is-active">
-        //             <div className="navbar-start">
-        //                 <Link to="/">
-        //                     <button className="navbar-item button is-primary space">
-        //                         Veggie recepti
-        //                     </button>
-        //                 </Link>
-        //                 <Link to="/vip">
-        //                     <button className="navbar-item button is-primary space">
-        //                         VIP recepti
-        //                     </button>
-        //                 </Link>
-        //             </div>
-
-        //             <button
-        //                 className="buttonClose"
-        //                 onClick={() => {
-        //                     openRecipeForm();
-        //                 }}
-        //             >
-        //                 <strong>DODAJ RECEPT</strong>
-        //             </button>
-        //             {newRecipe && (
-        //                 <CreateRecipeModal
-        //                     update={update}
-        //                     openRecipeForm={openRecipeForm}
-        //                 />
-        //             )}
-        //         </div>
-        //     </nav>
-        // </div>
     )
 }
 
